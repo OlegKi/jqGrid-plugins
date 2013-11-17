617dd15728dcc0c1898d54eac1f6690cb2136091
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2013-11-17 20:57:20 +0100

    setColWidth method for jqGrid
    The method allows to change the width of any column of the grid *after*
    the grid is created.

---
+++ jQuery.jqGrid.setColWidth.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2012, Dr. Oleg Kiriljuk, oleg.kiriljuk@ok-soft-gmbh.com
+ * Dual licensed under the MIT and GPL licenses
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ * Date: 2013-11-17
+ * see the answers http://stackoverflow.com/a/20030652/315935
+ *             and http://stackoverflow.com/a/14265456/315935
+ */
+
+/*global jQuery */
+(function ($) {
+    "use strict";
+    /*global $ */
+    /*jslint plusplus: true */
+    $.jgrid.extend({
+        setColWidth: function (iCol, newWidth, adjustGridWidth) {
+            return this.each(function () {
+                var $self = $(this), grid = this.grid, colName, colModel, i, nCol;
+                if (typeof iCol === "string") {
+                    // the first parametrer is column name instead of index
+                    colName = iCol;
+                    colModel = $self.jqGrid("getGridParam", "colModel");
+                    for (i = 0, nCol = colModel.length; i < nCol; i++) {
+                        if (colModel[i].name === colName) {
+                            iCol = i;
+                            break;
+                        }
+                    }
+                    if (i >= nCol) {
+                        return; // error: non-existing column name specified as the first parameter
+                    }
+                } else if (typeof iCol !== "number") {
+                    return; // error: wrong parameters
+                }
+                grid.resizing = { idx: iCol };
+                grid.headers[iCol].newWidth = newWidth;
+                grid.newWidth = grid.width + newWidth - grid.headers[iCol].width;
+                grid.dragEnd();   // adjust column width
+                if (adjustGridWidth !== false) {
+                    $self.jqGrid("setGridWidth", grid.newWidth, false); // adjust grid width too
+                }
+            });
+        }
+    });
+}(jQuery));
