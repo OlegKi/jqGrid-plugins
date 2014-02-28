95a3b94c12c4cab2716b3dbe8405153408821eef
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2014-02-28 13:44:08 +0100

    Update jQuery.jqGrid.setColWidth.js

--- jQuery.jqGrid.setColWidth.js
+++ jQuery.jqGrid.setColWidth.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012, Dr. Oleg Kiriljuk, oleg.kiriljuk@ok-soft-gmbh.com
+ * Copyright (c) 2013, Dr. Oleg Kiriljuk, oleg.kiriljuk@ok-soft-gmbh.com
  * Dual licensed under the MIT and GPL licenses
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl-2.0.html
