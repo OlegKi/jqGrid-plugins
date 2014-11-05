fb2bbda6e23146c3f2ff83da4ce3bcb818ba53cf
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2014-11-05 15:34:15 +0100

    rewritten version of autoWidthColumns
    the new version wraps cells contain in `<span
    class='mywrapping'></span>` only *temporary*. The span will be removed
    immediately after measuring of its width.

--- jQuery.jqGrid.autoWidthColumns.js
+++ jQuery.jqGrid.autoWidthColumns.js
@@ -4,46 +4,64 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl-2.0.html
  * Date: 2014-10-19
- * see http://stackoverflow.com/a/24329916/315935 and http://stackoverflow.com/q/26437736/315935 for more details
+ * see http://stackoverflow.com/a/24329916/315935
+ * and http://stackoverflow.com/q/26437736/315935
+ * and http://stackoverflow.com/a/26753600/315935 for more details
  */
 /*global jQuery */
 (function ($) {
 	"use strict";
-	/*jslint continue: true, plusplus: true */
+	/*jslint eqeq: true, unparam: true, todo: true, plusplus: true */
 	$.jgrid.extend({
 		autoWidthColumns: function () {
 			return this.each(function () {
-				$(this).bind("jqGridAfterLoadComplete jqGridRemapColumns", function () {
-					var $this = $(this),
-						$cells = $this.find(">tbody>tr.jqgrow>td"),
-						$colHeaders = $($.map(this.grid.headers, function (item) { return item.el; })).find(">div"),
-						colModel = $this.jqGrid("getGridParam", "colModel"),
-						cellLayout = $this.jqGrid("getGridParam", "cellLayout"),
-						iCol,
-						iRow,
-						rows,
-						row,
-						n = $.isArray(colModel) ? colModel.length : 0,
-						cm,
-						colWidth,
-						idColHeadPrexif = "jqgh_" + this.id + "_";
+				var recalculateColumnWidth = function (iCol, adjustGridWidth) {
+						// TODO: include support of more sophisticated grid features like frozen columns, grouping and so on.
+						var $self = $(this), iRow, rows, row, colWidth,
+							p = $self.jqGrid("getGridParam"),
+							cm = p.colModel[iCol],
+							getOuterWidth = function ($elem) {
+								var $wrappingSpan, width;
 
-					$cells.wrapInner("<span class='mywrapping'></span>");
-					$colHeaders.wrapInner("<span class='mywrapping'></span>");
+								$elem.wrapInner("<span class='mywrapping'></span>");
+								$wrappingSpan = $elem.find(">.mywrapping");
+								width = $wrappingSpan.outerWidth();
+								$elem.html($wrappingSpan.html());
 
-					for (iCol = 0; iCol < n; iCol++) {
-						cm = colModel[iCol];
-						if (cm.hidden) {
-							continue;
+								return width;
+							};
+
+						if (cm == null || cm.hidden) {
+							return; // don't change the width of hidden columns
 						}
-						colWidth = $("#" + idColHeadPrexif + $.jgrid.jqID(cm.name) + ">.mywrapping").outerWidth(true) + 25; // 25px for sorting icons
+						colWidth = getOuterWidth($(this.grid.headers[iCol].el).find(">div")) +
+							(cm.name === p.sortname || p.viewsortcols[0] ? 20 : 0) +
+							(!$.jgrid.cell_width ? p.cellLayout : 0); // 25px for sorting icons
 						for (iRow = 0, rows = this.rows; iRow < rows.length; iRow++) {
 							row = rows[iRow];
 							if ($(row).hasClass("jqgrow")) {
-								colWidth = Math.max(colWidth, $(row.cells[iCol]).find(".mywrapping").outerWidth(true));
+								colWidth = Math.max(colWidth, getOuterWidth($(row.cells[iCol])));
 							}
 						}
-						$this.jqGrid("setColWidth", iCol, colWidth + ($.jgrid.cell_width ? cellLayout : 0));
+						if (colWidth !== cm.width) {
+							colWidth += ($.jgrid.cell_width ? p.cellLayout : 0);
+							$self.jqGrid("setColWidth", iCol, colWidth, adjustGridWidth);
+						}
+					},
+					autosizeAllColumns = function (adjustGridWidth) {
+						var iCol,
+							colModel = $(this).jqGrid("getGridParam", "colModel"),
+							n = $.isArray(colModel) ? colModel.length : 0;
+
+						for (iCol = 0; iCol < n; iCol++) {
+							recalculateColumnWidth.call(this, iCol, adjustGridWidth);
+						}
+					};
+
+				$(this).bind("jqGridAfterLoadComplete jqGridInlineAfterSaveRow", autosizeAllColumns);
+				$(this).bind("jqGridShowHideCol", function (e, isShow, columnName, iCol) {
+					if (isShow) {
+						recalculateColumnWidth.call(this, iCol, true);
 					}
 				});
 			});
