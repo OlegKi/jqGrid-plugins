d38bb10a15319108f00f96d22e3762f96ade21a9
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2014-11-05 16:40:55 +0100

    year in Copyright

--- jQuery.jqGrid.autoWidthColumns.js
+++ jQuery.jqGrid.autoWidthColumns.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013, Oleg Kiriljuk, oleg.kiriljuk@ok-soft-gmbh.com
+ * Copyright (c) 2014, Oleg Kiriljuk, oleg.kiriljuk@ok-soft-gmbh.com
  * Dual licensed under the MIT and GPL licenses
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl-2.0.html
