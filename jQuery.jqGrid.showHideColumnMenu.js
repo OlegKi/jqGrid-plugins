2391e76051296a31d31668ab2c5ada77af094f38
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2014-11-13 12:29:13 +0100

    introduction modifyMenuItem callback and viewHideDlgColumnsAsDisabled option

--- jQuery.jqGrid.showHideColumnMenu.js
+++ jQuery.jqGrid.showHideColumnMenu.js
@@ -13,11 +13,26 @@
 	$.extend($.jgrid, {
 		showHideColumnMenu: {
 			adjustGridWidth: true,
+			viewHideDlgColumnsAsDisabled: false,
 			shrink: false,
 			menuStyle: {float: "left"},
 			checkboxChecked: "<input disabled=\"disabled\" checked=\"checked\" type=\"checkbox\"/>",
 			checkboxUnChecked: "<input disabled=\"disabled\" type=\"checkbox\"/>",
 			checkboxSelector: "input[type=checkbox]",
+			modifyMenuItem: function ($li, cm, options) {
+				if ($.inArray(cm.name, ["rn", "subgrid", "cb"]) >= 0) { // skip predefined columns
+					$li.hide();
+					return;
+				}
+				if (!cm.hidedlg) {
+					return;
+				}
+				if (options.viewHideDlgColumnsAsDisabled) {
+					$li.addClass("ui-state-disabled");
+				} else {
+					$li.hide();
+				}
+			},
 			isChecked: function ($checkbox) { return $checkbox.is(":checked"); },
 			toCheck: function ($checkbox) { $checkbox.prop("checked", true); },
 			toUnCheck: function ($checkbox) { $checkbox.prop("checked", false); }
@@ -34,15 +49,10 @@
 						$menu = $("<ul></ul>");
 					for (iCol = 0; iCol < nCol; iCol++) {
 						cm = colModel[iCol];
-						if ($.inArray(cm.name, ["rn", "subgrid", "cb"]) >= 0) { // skip predefined columns
-							continue;
-						}
 						$li = $("<li></li>")
 								.data("iCol", iCol)
 								.text(colNames[iCol] === "" ? cm.name : colNames[iCol]);
-						if (cm.hidedlg) {
-							$li.addClass("ui-state-disabled");
-						}
+						options.modifyMenuItem.call($self[0], $li, cm, options);
 						$li.prepend(cm.hidden ? options.checkboxUnChecked : options.checkboxChecked);
 						$li.appendTo($menu);
 					}
@@ -52,15 +62,16 @@
 							select: function (event, ui) {
 								var i = parseInt(ui.item.data("iCol"), 10), $cb = ui.item.find(options.checkboxSelector),
 									colWidth = $self[0].grid.headers[i].width + (!$.jgrid.cell_width ? p.cellLayout : 0),
-									toHide = options.isChecked($cb, event);
-								if (!isNaN(i) && i >= 0 && colModel[i] !== undefined && $cb.length > 0) {
+									cmi = colModel[i],
+									toHide = options.isChecked.call($self[0], $cb, event, cmi);
+								if (!isNaN(i) && i >= 0 && cmi !== undefined && $cb.length > 0) {
 									if (toHide) {
-										options.toUnCheck($cb, event);
+										options.toUnCheck.call($self[0], $cb, event, cmi);
 										colWidth = $($self[0].grid.headers[i].el).outerWidth();
-										$self.jqGrid("hideCol", colModel[i].name);
+										$self.jqGrid("hideCol", cmi.name);
 									} else {
-										options.toCheck($cb, event);
-										$self.jqGrid("showCol", colModel[i].name);
+										options.toCheck.call($self[0], $cb, event, cmi);
+										$self.jqGrid("showCol", cmi.name);
 										colWidth = $($self[0].grid.headers[i].el).outerWidth();
 									}
 									$(this).parent().css("zoom", 1); // fix visibility in IE
